18a711b4c08b75475213869afa5c198cd3ae44f8
domara <domara.best@gmail.com>
2025-08-16 13:21:26 +0300

    added getAllDentists function

--- controllers/dentistController.js
+++ controllers/dentistController.js
@@ -3,7 +3,7 @@ import { createDentist,getDentistById,getAllDentist,updateDentist,deleteDentist 
 
 
 //create a new dentist
-export const createDentist=(req,res)=>{
+export const createDentist=async(req,res)=>{
     try{
         const userid=req.user.userId;
 
@@ -14,7 +14,7 @@ export const createDentist=(req,res)=>{
         }=req.body;
 
 
-        const newDentist=createDentist(userid,specialization,clinic_name,clinic_address);
+        const newDentist=await createDentist(userid,specialization,clinic_name,clinic_address);
 
         if(!newDentist) return res.status(404).json({message:'User was not created'});
 
@@ -33,5 +33,30 @@ export const createDentist=(req,res)=>{
         res.status(501).json({error:err.message,
             message:'Server error, could not create dentist'
         })
+    }
+}
+
+
+//get all dentists 
+export const get_All_dentists=async(req,res)=>{
+    try{
+
+        //query db for all dentist
+        const getDents=await getAllDentist();
+
+        if(!getDents) return res.status(404).json({message:'Could not get dentists'});
+
+        res.status(201).json({message:'Operation retrieving dentists successful' , getDents})
+
+
+    }
+    catch(err){
+        res.status(501).json({
+            error:err.message,
+            message:"Internal server error"
+        })
+
+
+
     }
 }
