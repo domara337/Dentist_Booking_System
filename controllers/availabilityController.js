aa94a774c9139de324f2d908b1445da26e13a913
domara <domara.best@gmail.com>
2025-08-18 15:20:56 +0300

    added createAvailability controller function

--- controllers/availabilityController.js
+++ controllers/availabilityController.js
@@ -3,6 +3,70 @@ import { createAvailablity,getAvailablityByDentist,updateAvailablity,deleteAvail
 
 
 
+
+
+
+//create availabilityslot
+export const createAvailability=async(req,res)=>{
+
+try{
+
+    //get dentist id from the req.params
+    const dentId=req.params.id;
+
+    //get the variables from the req.body
+    const{
+        day_of_week,start_time,end_time
+    }=req.body
+
+
+
+
+    ///insert new availability in db
+    const NewAvailability=await createAvailability(dentId,day_of_week,start_time,end_time);
+
+    if(NewAvailability) return res.status(201).json({
+        message:"availability slot was created successfully",
+    NewAvailability})
+
+
+
+
+
+
+
+
+}
+catch(err){
+    res.status(501).json({
+        error:err.message,
+        message:"Internal server error"
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 //get availablity by the dentist id
 export const getAvailability=async(req,res)=>{
     try{
@@ -35,3 +99,5 @@ export const getAvailability=async(req,res)=>{
         })
     }
 }
+
+
