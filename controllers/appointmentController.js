f13cbe0a95273673dc6cffcfa0fcc283a4fc2a1b
domara <domara.best@gmail.com>
2025-08-22 11:29:23 +0300

    modified appointmentsModel and added get_appointment_by_id controller function

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,3 +1,4 @@
+import pool from "../config/db";
 import { createAppointment,getAppointmentsByuser,getAppointmentsBydate,getAllApointments,
     getAppointmentByDentist,updateAppointment,
     deleteAppointment
@@ -56,6 +57,7 @@ import { createAppointment,getAppointmentsByuser,getAppointmentsBydate,getAllApo
       SELECT 
         a.id AS appointment_id,
         a.date,
+        a.status,
         p.id AS patient_id,
         p.name AS patient_name,
         p.email AS patient_email,
@@ -74,6 +76,61 @@ import { createAppointment,getAppointmentsByuser,getAppointmentsBydate,getAllApo
     console.error(error);
     res.status(500).json({ message: "Error fetching appointments" });
   }
+
+
+
+ }
+
+
+
+ //get appointment by Id function 
+ export const get_appointment_byId=async(req,res)=>{
+
+try{
+
+//get the id from the req.params
+const {id}=req.params.id;
+
+
+//query appointment from database
+const query=`
+SELECT 
+a.id AS appointment_id,
+a.date,
+a.status,
+p.id AS patient_id,
+p.name AS patient_name,
+p.email AS patient_email,
+d.id AS dentist_id,
+d.name AS dentist_name,
+d.speciality AS dentist_speciality,
+
+FROM appointments a
+ JOIN patients p ON a.patient_id = p.id
+      JOIN dentists d ON a.dentist_id = d.id
+      WHERE a.id = $1
+    `;
+
+const {rows}=await pool.query(query,[id])
+
+if(rows.length===0){
+    return res.status(404).json({messasge:"Appointment not found"})
+}
+
+return res.status(200).json(rows[0])
+
+
+
+}
+catch(err){
+
+    res.status(500).json({message:"Internal server error" , 
+        error:err.message
+    })
+
+
+}
+
 
 
 
